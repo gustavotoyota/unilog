813c3b64b9b400815305a696e6029ffad52bea3d
Gustavo Toyota <gustavottoyota@gmail.com>
2022-11-02 22:38:43 -0400

    fix(package.json): undo subfolder exports (doesn't work)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './info';
+export * from './levels';
 export * from './logger';
+export * from './operations';
+export * from './outputs';
 export * from './stream';
+export * from './symbols';
