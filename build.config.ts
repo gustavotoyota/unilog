c7ace78ce61009db4ebb28fae15c02c181b34a3c
Gustavo Toyota <gustavottoyota@gmail.com>
2022-11-03 15:16:08 -0400

    chore: bundle in a single file

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,5 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  entries: [
-    { input: 'src/', outDir: 'dist/', format: 'cjs', ext: 'cjs' },
-    { input: 'src/', outDir: 'dist/', format: 'esm' },
-  ],
   declaration: true, // generate .d.ts files
 });
